e0031b8ffa82f1e04589629815a26acce9a6d7d0
YuvalShor <10600102+YuvalShor@users.noreply.github.com>
2021-12-01 01:39:42 +0200

    Started the API for retrieving the user's hours

--- components/worksummary.js
+++ components/worksummary.js
@@ -1,8 +1,41 @@
 import * as React from "react";
+import { useState, useEffect } from "react";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
+import Cookies from "universal-cookie";
 
-const WorkSummary = () => {
+const cookies = new Cookies();
+
+const WorkSummary = ({ currentSessionTime }) => {
+  const [totalMonthlyHours, setTotalMonthlyHours] = useState(0);
+  const loggedInEmployee = cookies.get("user");
+
+  useEffect(async () => {
+    try {
+      const res = await fetch("/api/employees", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          phone: loggedInEmployee.phone,
+          data: "monthlyhours",
+        }),
+      });
+
+      console.log(currentSessionTime);
+
+      const { data: hoursFromDB } = await res.json();
+      console.log(hoursFromDB);
+      setTotalMonthlyHours(
+        parseInt(hoursFromDB) + parseInt(currentSessionTime)
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
   return (
     <div
       style={{
@@ -17,11 +50,13 @@ const WorkSummary = () => {
           direction="column"
           style={{ height: "10vh", marginTop: "50%" }}
         >
-          <Typography variant="h6">Total work hours this month:</Typography>
+          <Typography variant="h6">
+            Total work hours this month: {totalMonthlyHours}
+          </Typography>
         </Grid>
       </div>
     </div>
   );
-}
+};
 
 export default WorkSummary;
